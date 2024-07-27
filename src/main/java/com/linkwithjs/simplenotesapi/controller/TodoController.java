546ce45efd2f2d40bdf187ba9fdc901156d808c4
Rajendra Lungeli <linkwithjs@gmail.com>
2024-07-27 23:34:33 +0545

    Todo list CRUD done.

--- src/main/java/com/linkwithjs/simplenotesapi/controller/TodoController.java
+++ src/main/java/com/linkwithjs/simplenotesapi/controller/TodoController.java
@@ -1,14 +1,13 @@
 package com.linkwithjs.simplenotesapi.controller;
 
+import com.linkwithjs.simplenotesapi.dto.ReqRes;
 import com.linkwithjs.simplenotesapi.dto.TodoDTO;
+import com.linkwithjs.simplenotesapi.dto.TopicDTO;
 import com.linkwithjs.simplenotesapi.service.TodoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequiredArgsConstructor
@@ -16,8 +15,30 @@ import org.springframework.web.bind.annotation.RestController;
 public class TodoController {
     @Autowired
     TodoService todoService;
+
     @PostMapping("/create-todo")
-    public ResponseEntity<?> createTodo(@RequestBody TodoDTO todos){
+    public ResponseEntity<?> createTodo(@RequestBody TodoDTO todos) {
         return ResponseEntity.ok(todoService.createTodo(todos));
     }
+
+    @GetMapping("/get-todos")
+    public ResponseEntity<?> getAllTodos() {
+        return ResponseEntity.ok(todoService.getAllTodos());
+    }
+
+    @PatchMapping("/todo-completed/{id}")
+    public ResponseEntity<?> todoCompleted(@PathVariable(value = "id") int id) {
+        return ResponseEntity.ok(todoService.todoCompleted(id));
+    }
+
+    @DeleteMapping("delete-todo/{id}")
+    public ResponseEntity<?> deleteTodo(@PathVariable(value = "id") int id) {
+        return ResponseEntity.ok(todoService.deleteTodo(id));
+    }
+
+    @PutMapping("/update-todo/{id}")
+    public ResponseEntity<ReqRes> updateTodo(@PathVariable(value="id") int todoId, @RequestBody TodoDTO todos){
+        return ResponseEntity.ok(todoService.updateTodo(todoId, todos));
+    }
+
 }
--- src/main/java/com/linkwithjs/simplenotesapi/service/TodoService.java
+++ src/main/java/com/linkwithjs/simplenotesapi/service/TodoService.java
@@ -3,28 +3,103 @@ package com.linkwithjs.simplenotesapi.service;
 import com.linkwithjs.simplenotesapi.dto.ReqRes;
 import com.linkwithjs.simplenotesapi.dto.TodoDTO;
 import com.linkwithjs.simplenotesapi.entity.TodoEntity;
+import com.linkwithjs.simplenotesapi.exception.CustomException;
 import com.linkwithjs.simplenotesapi.repository.TodoRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
 
 @Service
 public class TodoService {
     @Autowired
     TodoRepository todoRepository;
+    @Autowired
+    private ModelMapper modelMapper;
 
-    public ReqRes createTodo(TodoDTO todos){
+    public ReqRes createTodo(TodoDTO todos) {
         ReqRes resp = new ReqRes();
-        try{
+        try {
             TodoEntity todoEntity = new TodoEntity();
             todoEntity.setTitle(todos.getTitle());
             TodoEntity saveTodo = todoRepository.save(todoEntity);
-            if(saveTodo.getId()!=null){
+            if (saveTodo.getId() != null) {
                 resp.setData(saveTodo);
                 resp.setMessage("Todo added successfully.");
-            }else {
+            } else {
                 resp.setMessage("Todo could not add.");
             }
-        }catch (Exception e){
+        } catch (Exception e) {
+            resp.setStatusCode(500);
+            resp.setError(e.getMessage());
+        }
+        return resp;
+    }
+
+    public ResponseEntity<?> getAllTodos() {
+        try {
+            List<TodoDTO> todos = todoRepository.findAll().stream()
+                    .map(todo -> modelMapper.map(todo, TodoDTO.class))
+                    .collect(Collectors.toList());
+            return ReqRes.successResponse("Todos fecthed successfully.", todos);
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR)
+                    .body(ReqRes.successResponse("Failed to fetch topics", e.getMessage()));
+        }
+    }
+
+    public ReqRes todoCompleted(int id) {
+        ReqRes resp = new ReqRes();
+        try {
+            TodoEntity todo = todoRepository.findById(id).orElseThrow(() ->
+                    new CustomException("Todo not found for this id: " + id));
+            todo.setCompleted(!todo.isCompleted());
+            todoRepository.save(todo);
+            resp.setData(todo);
+            resp.setMessage("Todo completed.");
+        } catch (Exception e) {
+            resp.setStatusCode(500);
+            resp.setError(e.getMessage());
+        }
+        return resp;
+    }
+
+    public ReqRes deleteTodo(int id) {
+        ReqRes resp = new ReqRes();
+        try {
+            TodoEntity todo = todoRepository.findById(id).orElseThrow(() -> new CustomException("Topic not found for this id: " + id));
+            todoRepository.delete(todo);
+            resp.setStatusCode(200);
+            resp.setData(todo);
+            resp.setMessage("Todo Deleted Successfully.");
+        } catch (CustomException e) {
+            resp.setStatusCode(404);
+            resp.setError(e.getMessage());
+        } catch (Exception e) {
+            resp.setStatusCode(500);
+            resp.setError(e.getMessage());
+        }
+        return resp;
+    }
+
+    public ReqRes updateTodo(int id, TodoDTO todo) {
+        ReqRes resp = new ReqRes();
+        try {
+            TodoEntity todos = todoRepository.findById(id).orElseThrow(() ->
+                    new CustomException("Todo not found for this id: ", id));
+            todos.setTitle(todo.getTitle());
+            TodoEntity saveTodo = todoRepository.save(todos);
+            if (saveTodo.getId() != null) {
+                resp.setData(saveTodo);
+                resp.setMessage("Todo updated successfully.");
+            } else {
+                resp.setMessage("Todo could not update.");
+            }
+        } catch (Exception e) {
             resp.setStatusCode(500);
             resp.setError(e.getMessage());
         }
